cb63ae33f19ba4ace9066737ae850ea4ec3259e0
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-12-30 14:55:17 +0300

    0.0.1-0009

--- exodus-survival/src/client/java/com/exodus/survival/client/ExodusSurvivalClient.java
+++ exodus-survival/src/client/java/com/exodus/survival/client/ExodusSurvivalClient.java
@@ -1,10 +1,14 @@
 package com.exodus.survival.client;
 
+import com.exodus.survival.client.health.BodyHealthHud;
+import com.exodus.survival.client.health.network.ClientNetworkHandler;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ExodusSurvivalClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ClientNetworkHandler.register();
+        BodyHealthHud.register();
     }
 }
